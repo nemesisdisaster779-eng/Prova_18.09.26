9ad347fadd416fbdb25f5f46e0b6be4aa66be508
Nemesis_Disater <nemesisdisaster779@gmail.com>
2026-09-20 14:49:06 -0300

    Só pra salvar o que fiz de jss

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,42 @@
-<main id="app">
-querySelectorall()
-</main>
+let eventos = [
+    {
+        id: 1,
+        titulo: "Aulas avançadas de C#",
+        tipo: "Curso",
+        data: "2027-02-15",
+        horario: "Das 09:30 as 15:15",
+        local: "Laboratorio de Braile",
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.", 
+        status: "Agendado"
+},
+{
+    id: 2,
+    titulo: "Palestra sobre o Setembro Amarelo",
+    tipo: "Palestra",
+    data: "2026-09-18",
+    horario: "Das 09:30 as 13:00",
+    local: "Auditorio",
+    descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.", 
+    status: "Realizado"
+},
+{
+    id: 3,
+    titulo: "Aulas praticas de rede de computadores",
+    tipo: "Curso",
+    data: "2027-03-20",
+    horario: "Das 14:00 as 18:00",
+    local: "Laboratorio 3",
+    descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.", 
+    status: "Agendado"
+},
+{
+    id: 4,
+    titulo: "Palestra sobre o Outubro Rosa",
+    tipo: "Palestra", 
+    data: "2026-10-20",
+    horario: "Das 09:30 as 13:00",
+    local: "Auditorio",
+    descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.", 
+    status: "Agendado"     
+}
+]
